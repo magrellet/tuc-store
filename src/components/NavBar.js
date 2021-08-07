ef43305d99547510a7c372f1ee649480c77d556f
Martin Alejandro Grellet <mgrellet@MacBook-Pro-de-Martin.local>
2021-08-07 11:57:13 -0300

    02 - Adding NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+import "./NavBar.css";
+
+class NavBar extends Component {
+  render() {
+    const menuItems = ["Marcas", "Productos", "Sale!"];
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h4">Tuc Store e-commerce</Typography>
+          <section className="Menu-Button">
+            {menuItems.map((item, i) => {
+              return <Button color="inherit">{item}</Button>;
+            })}
+          </section>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+export default NavBar;
